f1187d1068b857e29221ddd3d1f0d62bfa9e035f
Luke Swart <lukeswart@gmail.com>
2015-05-06 02:58:18 +0000

    Add link file script

---
+++ linkFiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/dotfiles-remote/.bashrc ~/.bashrc
+ln -s ~/dotfiles-remote/.gitignore ~/.gitignore
+ln -s ~/dotfiles-remote/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles-remote/.tmux.conf ~/.tmux.conf
