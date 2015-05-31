040dbcd5a0049539fce8e38e8583f5a29c5c2d19
Luke Swart <lukeswart@gmail.com>
2015-05-31 01:38:40 +0000

    Add .vimrc to link files script

--- linkFiles.sh
+++ linkFiles.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles-remote/.bashrc ~/.bashrc
 ln -s ~/dotfiles-remote/.gitignore ~/.gitignore
 ln -s ~/dotfiles-remote/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles-remote/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles-remote/.vimrc ~/.vimrc
